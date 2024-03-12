e15eb7878b27e81dcde5db269ccc7faa9407ad4c
Runquan (Ray) Zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-03-11 21:30:40 -0400

    add submit form

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const task = event.target.toDos.value;
+
+    const ul = document.querySelector("ul")
+
+    const li = document.createElement("li")
+
+    li.append(task)
+
+    ul.append(li)
+
+    console.log(task)
+
+    form.reset()
+})
