6a105df93bd943d836d3d17f2b5d9d57a5d1058a
gerardjr42 <ggarciajr12@gmail.com>
2024-03-13 15:11:23 -0400

    add delete button to each to-do list

--- index.js
+++ index.js
@@ -29,21 +29,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const text = $(".text-area");
     const ul = $("ul");
     const p = $("p");
+
+  
     
     form.on("submit", (event) => {
       event.preventDefault();
 
+      //Create li
       const li = create("li");
-      li.setAttribute("class", "list")
+      li.setAttribute("class", "list");
 
+      //Create delete button
+      const delBtn = create("button");
+      delBtn.setAttribute("class", "delete");
+      delBtn.innerText = "Delete";
+      
+      //If no entry, output message
       if(text.value === '') {
-        p.innerHTML = `
-          Error! No Text Input Detected. <br> Please input text before submitting.
-        `;
+        p.innerHTML = `Error! No Text Input Detected. <br> Please input text before submitting.`;
       } else {
         p.innerText = '';
         li.textContent = text.value;
         ul.appendChild(li);
+        li.appendChild(delBtn);
       }
       form.reset();
     })
@@ -51,9 +59,21 @@ document.addEventListener("DOMContentLoaded", () => {
     //Function to Cross Out To-Do Lists.
     //Used Event Bubbling: https://typeofnan.dev/how-to-bind-event-listeners-on-dynamically-created-elements-in-javascript/  
     //Add a ternary for toggling line-through
-    ul.on("click", function(e) {
+    ul.on("dblclick", function(e) {
       if(e.target.classList.contains("list")) {
         e.target.style.textDecoration === "line-through" ? e.target.style.textDecoration = 'none' : e.target.style.textDecoration = "line-through"
       }
+    });
+
+    //Add functionality to our delete button
+    //Can we add it above??
+
+    
+    //Below deletes the Button not the list
+    ul.on("click", function(e) {
+      if(e.target.classList.contains("delete")) {
+        e.target.remove();
+      }
     })
+
 });
